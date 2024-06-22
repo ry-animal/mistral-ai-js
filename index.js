ff5101a4ca3eeb541b1a7feae2254a8279d5383c
ryanimal <ryanlvv@gmail.com>
2024-06-22 11:56:09 -0700

    working

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import MistralClient from '@mistralai/mistralai';
 
 const apiKey = process.env.MISTRAL_API_KEY || '';
 const client = new MistralClient(apiKey);
+
+const chatResponse = await client.chat({
+    model: "mistral-tiny",
+    messages: [{role: 'user', content: 'ASK Q HERE'}]
+});
+
+console.log(chatResponse.choices[0].message.content);
